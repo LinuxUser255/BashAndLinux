d55fab6d50abe966e5cfa96c181159ca7638b7ec
Chris <atnl@protonmail.com>
2021-07-09 15:02:40 -0400

    Update and rename smkdir.sh to easy_mkdir.sh

--- Useful_Scripts/easy_mkdir.sh
+++ Useful_Scripts/easy_mkdir.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# I want to skip the typing of:  sudo mkdir
-# Therefore, create a shortcut to enable the creation of a new directory as a non-root user.
+# GOAL: Create a new directory fast..Eliminate the need of typing  sudo mkdir
+# Therefore, enable the creation of a new directory as a non-root user.
 # Then, rename it without the .sh and give it a super short name to run it FAST, then move it to /usr/bin
 
 read -p 'Enter the new dir name: ' NEW_DIRECTORY
