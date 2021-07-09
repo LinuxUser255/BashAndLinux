d5f58b0cb351384ede8e67c6268cd146cdb2d763
Chris <atnl@protonmail.com>
2021-07-09 16:10:48 -0400

    Update dd_backup.sh

--- Useful_Scripts/dd_backup.sh
+++ Useful_Scripts/dd_backup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# Flash OS and all data to an external gard drive.
+echo '! READ COMMENTS before executing the script!'
+echo ' ! Hit Control  c to exit !'
+# Flash OS and all data to an external hard drive.
 # Comment out all lines but 22 if you plan to automate backups via crontab.
 # Leave as is to run script manualy. 
+# Important. Identify and confirm the device to which you ae writing data, 
+# otherwise you could inadvertently erase the wrong drive.
+# This example assumes the device to write to is /dev/sdb
+
 echo ' '
 echo -e " Run lsblk first? Hit 1 for Yes. Hit 2 for No: \n1 yes\n2 no\n"
 read sel
+
+# You could also run fdisk -l for extra confirmation and info
+# More options could be added to this script such as: Run fdisk -l (yes or no)
+# Maybe a while loop, a variable/place holder for the device's name  /dev/sd $DEVICE_NAME
+# Give an option to type in the device's name, /dev/sdb , sdc, etc..
+# And, add a confirmation of the device you chose and a selection to run the script now, or cancel.
 
 # Hit 1 for lsblk.
 if [[ $sel -eq 1 ]]; 
@@ -20,4 +32,8 @@ true
 
 fi
 
-dd if=/dev/sda of=/dev/sdb
+# I've commented out the dd command to prevent accidental execution.
+# Do not uncomment the dd cmd and run this scipt, until you are ABSOLUTELY CERTAIN..
+# And, have POSITIVELY IDENTIFIED, the device you want to write to.
+
+# dd if=/dev/sda of=/dev/sdb
