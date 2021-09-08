109b630403c92a41930f02fc05041b12998b82df
Chris <atnl@protonmail.com>
2021-09-07 23:37:26 -0400

    Create window_size.sh

---
+++ UsrBin/window_size.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ https://github.com/dylanaraps/pure-bash-bible.git
+
+get_window_size() {
+    # Usage: get_window_size
+    printf '%b' "${TMUX:+\\ePtmux;\\e}\\e[14t${TMUX:+\\e\\\\}"
+    IFS=';t' read -d t -t 0.05 -sra term_size
+    printf '%s\n' "${term_size[1]}x${term_size[2]}"
+}
+
+
+get_window_size
