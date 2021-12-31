e31a85987696f5af86cc4e2a3aeb53cc31bed7e5
Chris <atnl@protonmail.com>
2021-12-31 16:48:34 -0500

    Update print-fields-for-awk.sh

--- Regex/print-fields-for-awk.sh
+++ Regex/print-fields-for-awk.sh
@@ -5,8 +5,8 @@
 # This enables you to print as many values as needed, then copy-paste them into the awk brackets.
 # Alternatively, it can be run as a One-Liner in the command line:
 # for i in  {1..20}; printf  $i"\t" | sed "s/\s/,$/g;"
-# result: 1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$
-for i in {1..20}; do
+# 0,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$
+for i in {0..20}; do
 	printf $i"\t" | sed "s/\s/,$/g;"
 
 done
