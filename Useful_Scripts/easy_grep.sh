a19c6ec118eeb6c95ffc8bc741a2736eee9070a8
Chris <atnl@protonmail.com>
2022-03-22 13:30:36 +0000

    Update easy_grep.sh

--- Useful_Scripts/easy_grep.sh
+++ Useful_Scripts/easy_grep.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Stop typing ls | grep -i "search_pattern" everytime you want to search a directory.
-# Search your directories with a super short & sweet cmmd.
-# Create this bash script, rename it (remove the .sh), and put it in /usr/bin
-# Then, create an alias for it in your .bashrc (I made it lll).
-# Now,  inorder to search a directory, all you gotta do is tap the letter l three times,(lll), hit enter & BOOM! Off you go'
+# I replaced grep with ripgrep
+# It's like grep but faster, and other neat features too.
+# Debian users:  sudo apt-get install ripgrep
+# Arch users:  pacman -S ripgrep
+# For more info, see the repo: https://github.com/BurntSushi/ripgrep
 
-read -p 'ls | grep -i "' GREP  
+read -p 'ls | grep -i "' RG  
 echo ''
 echo -e 'Search result(s):\n------------------\n'
 
-ls | grep -i "${GREP}"
+ls | rg -i "${RG}"
 echo ''
 
 
