c32dd905f31760b35a978ae53a6b8ebbf6bc06e4
Chris <atnl@protonmail.com>
2022-11-29 18:11:59 -0500

    Create conditionals.lua

---
+++ Lua/conditionals.lua
@@ -0,0 +1,17 @@
+-- Number comparisons
+local age = 10
+
+if age > 18 then
+  print("over 18") -- this will not be executed
+end
+
+-- elseif and else
+age = 20
+
+if age > 18 then
+  print("over 18")
+elseif age == 18 then
+  print("18 huh")
+else
+  print("kiddo")
+end
