4b0ac9f8667058daec5520c461cd39730e285fe4
Chris <atnl@protonmail.com>
2022-11-29 18:12:51 -0500

    Create statement_combine.lua

---
+++ Lua/statement_combine.lua
@@ -0,0 +1,9 @@
+local age = 22
+
+if age == 10 and x > 0 then -- both should be true
+  print("kiddo!")
+elseif x == 18 or x > 18 then -- 1 or more are true
+  print("over 18")
+end
+
+-- result: over 18
