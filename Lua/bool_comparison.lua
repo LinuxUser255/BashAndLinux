d2630c872e1d4733d4fc0d99a52c863966ca7f37
Chris <atnl@protonmail.com>
2022-11-29 18:12:27 -0500

    Create bool_comparison.lua

---
+++ Lua/bool_comparison.lua
@@ -0,0 +1,13 @@
+-- Boolean comparison
+local isAlive = true
+
+if isAlive then
+    print("Be grateful!")
+end
+
+-- String comparisons
+local name = "sid"
+
+if name ~= "sid" then
+  print("not sid")
+end
