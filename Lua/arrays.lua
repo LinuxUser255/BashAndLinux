c68835fa7e45c62afbd19a72eb0d2fc2801bf323
Chris <atnl@protonmail.com>
2022-11-29 18:14:56 -0500

    Create arrays.lua

---
+++ Lua/arrays.lua
@@ -0,0 +1,24 @@
+local colors = { "red", "green", "blue" }
+
+print(colors[1]) -- red
+print(colors[2]) -- green
+print(colors[3]) -- blue
+
+-- Different ways to loop through lists
+-- #colors is the length of the table, #tablename is the syntax
+
+for i = 1, #colors do
+  print(colors[i])
+end
+
+-- ipairs 
+for index, value in ipairs(colors) do
+   print(colors[index])
+   -- or
+   print(value)
+end
+
+-- If you dont use index or value here then you can replace it with _ 
+for _, value in ipairs(colors) do
+   print(value)
+end
