a55d3a29486d8837b600d9d0ffe10e79506f9946
Chris <atnl@protonmail.com>
2023-02-08 21:29:25 -0500

    Update oneliners.sh

--- Useful_Scripts/oneliners.sh
+++ Useful_Scripts/oneliners.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+
+# This is not intended to be run as a bash script as is. just comment out what you dont want to use and or edit to your needs
+
+# Create multiple file
+for i in {1..9}; touch file${i}.txt
+
 # Create 30 directories in the format sec_01 to sec_30
 
 # make dirs sec_01 to sec_09 using a one-liner for loop
-# for i in {1..9}; mkdir sec_0${i}
+for i in {1..9}; mkdir sec_0${i}
 
 # make dirs sec_10 to sec_20
-# for i in {10..19}; mkdir sec_${i}
+for i in {10..19}; mkdir sec_${i}
 
 # make a series of directories starting with sec_10 to sec_30
-# for i in {20..30}; mkdir sec_${i}
+for i in {20..30}; mkdir sec_${i}
 
 # combine all in one script:
 for ((i = 1 ; i < 9 ; i++)); do
