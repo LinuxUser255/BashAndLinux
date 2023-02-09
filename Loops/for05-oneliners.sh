0fec6a141c2e4def710ef8a95966aaadd1ff2c32
Chris <atnl@protonmail.com>
2023-02-08 21:49:07 -0500

    Create for05-oneliners.sh

---
+++ Loops/for05-oneliners.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+# This is not intended to be run as a bash script as is. just comment out what you dont want to use and or edit to your needs
+
+# Create multiple file
+for i in {1..9}; touch file${i}.txt
+
+# Create 30 directories in the format sec_01 to sec_30
+
+# make dirs sec_01 to sec_09 using a one-liner for loop
+for i in {1..9}; mkdir sec_0${i}
+
+# make dirs sec_10 to sec_20
+for i in {10..19}; mkdir sec_${i}
+
+# make a series of directories starting with sec_10 to sec_30
+for i in {20..30}; mkdir sec_${i}
+
+# combine all in one script:
+for ((i = 1 ; i < 9 ; i++)); do
+    mkdir sec_0${i}
+
+done
+
+
+for ((i = 10 ; i < 19 ; i++)); do
+    mkdir sec_${i}
+
+done
+
+
+for ((i = 20 ; i < 30 ; i++)); do
+    mkdir sec_${i}
+
+done
