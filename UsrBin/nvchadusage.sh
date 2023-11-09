6bd1c4b4983658242f3304fc778a65d12b17f9a8
Chris <atnl@protonmail.com>
2023-11-09 10:58:32 -0500

    Create nvchadusage.sh

---
+++ UsrBin/nvchadusage.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo ""
+
+printf "\e[1;31m NvChad guide to using Nvim-tree.lua \e[0m";
+echo ""
+
+printf "\e[1;36m <https://nvchad.com/docs/features#nvim-tree_lua> \e[0m"
+echo ""
+echo ""
+
+
+printf "\e[1;31m * Open the editor with nvim inside a directory \e[0m";
+echo ""
+
+printf "\e[1;31m * Then execute the cmd\n
+      :NvimTreeFocus \e[0m";
+
+printf "\e[1;31m - This will open up the directory in an IDE format \e[0m";
+echo ""
+echo ""
+
+printf "\e[1;31m Moving around:  \e[0m";
+echo ""
+
+printf "\e[1;31m * j and k to navigate up and down the menu on the left \e[0m";
+echo ""
+
+printf "\e[1;31m * Jump right: ctrl + l  \e[0m"
+echo ""
+
+printf "\e[1;31m * Jump left: ctrl + h  \e[0m"
+echo ""
+echo ""
+
+printf "\e[1;31m * Tab through the open files:"
+echo ""
+
+printf "\e[1;31m * While the cursor is on the right side in an open file  \e[0m";
+echo ""
+
+printf "\e[1;31m * Press the TAB key to tab through each open file  \e[0m"
+echo ""
+echo ""
+
+printf "\e[1;31m To open another file: \e[0m"
+echo ""
+
+printf "\e[1;31m * Jump to the left side bar: ctrl + h \e[0m"
+echo ""
+
+printf "\e[1;31m * Nav up and down with j & k and press Enter to open a new file  \e[0m"
+echo ""
+echo ""
+#printf "\e[1;31m  \e[0m"
+#printf "\e[1;31m  \e[0m"
