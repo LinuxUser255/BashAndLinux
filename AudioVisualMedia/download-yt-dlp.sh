ab1990832a64d905b73ba28b1887d4959f82f371
Chris <atnl@protonmail.com>
2023-12-07 15:11:26 -0500

    Create download-yt-dlp.sh

---
+++ AudioVisualMedia/download-yt-dlp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://github.com/yt-dlp/yt-dlp/wiki/Installation
+# Install In UNIX-like OSes (MacOS, Linux, BSD)
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
