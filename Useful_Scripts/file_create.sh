b9fdfa70a3c1a4b34d260b612e83b52eafa7aecf
Chris <atnl@protonmail.com>
2024-02-27 15:59:03 -0500

    Update file_create.sh

--- Useful_Scripts/file_create.sh
+++ Useful_Scripts/file_create.sh
@@ -1,11 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# This script instantly creates & chmods a new file type of your choosing.
-# The primary reason I wrote this scrip:
-# I got tired of always having to type..
-# #!/usr/bin/env python3 or #!/bin/bash when writing scripts.
-
-# In this example you can create a Python, Bash, .txt or .md 
+# Instantly Create & chmod a new file type of your choosing.
+# In this example you can create a Python, Bash, Go, .txt or .md
 # Give it a short and easy to type new name, and remove the .sh extension
 # I named mine terp, as in, auto create & tell the interpreter what I want.
 # Then move it to /usr/bin
@@ -16,55 +12,73 @@
 # 3. The script is given executable privilages
 # 4. Then it is automatically opend in Vim and ready for writing
 
-# Make sure the script is being executed with sudo privileges.
-if [[ "${UID}" -ne 0 ]]
-then
-   echo 'Please run with sudo or as root.'
-   exit 1
-fi
 
-# Create either a Python, Bash, .txt file or a markdown doc & open it with Vim
-echo -e "Select a filetype to create: Python, Bash, txt, or md: \n1 = Python\n2 = Bash\n3 = txt\n4 = Markdown Doc File"
+# Create either a Python, Bash, Go, .txt file or a markdown doc & open it with NeoVim
+echo -e "Select a filetype to create: Python, Bash, Go, txt, or md: \n1 = Python\n2 = Bash\n3 = Go\n4 = txt\n5 = Markdown Doc File"
 read sel
 
-# Crete a Python file/script
+
+# 1. Crete a Python file/script
 if [[ $sel -eq 1 ]];
 then
     echo "Name the Python script: "
-    read NAME 
+    read NAME
 touch $NAME.py
 echo "#!/usr/bin/env python3" > $NAME.py
 chmod +x $NAME.py
-vim $NAME.py
+nvim $NAME.py
 
-# Create a Bash Script
+
+# 2. Create a Bash Script
 elif [[ $sel -eq 2 ]];
 then
     echo "Name the Bash script: "
     read NAME
 touch $NAME.sh
-echo "#!#!/usr/bin/env bash" > $NAME.sh
+echo "#!/bin/bash" > $NAME.sh
 chmod 755 $NAME.sh
-vim $NAME.sh
+nvim $NAME.sh
 
-# Create a .txt file
+
+# 3. Create a .Go file
 elif [[ $sel -eq 3 ]];
+then
+    echo "Name the Go script: "
+    read NAME
+
+touch $NAME.go
+echo "package main" > $NAME.go
+echo "" >> $NAME.go
+echo 'import "fmt"' >> $NAME.go
+echo "" >> $NAME.go
+echo "func main(){" >> $NAME.go
+echo "// put code here" >> $NAME.go
+echo "" >> $NAME.go
+echo "}" >> $NAME.go
+echo "" >> $NAME.go
+echo "" >> $NAME.go
+nvim $NAME.go
+
+
+# 4. Create a .txt file
+elif [[ $sel -eq 4 ]];
 then
     echo "Name the text file: "
     read NAME
 touch $NAME.txt
-echo "# ASCII Text" > $NAME.txt
-vim $NAME.txt
+echo "# Title" > $NAME.txt
+nvim $NAME.txt
 
-# Create a Markdown Documentation file
-elif [[ $sel -eq 4 ]];
+
+# 5. Create a Markdown Documentation file
+elif [[ $sel -eq 5 ]];
 then
     echo "Name the Markdown Doc file: "
     read NAME
 touch $NAME.md
-echo "# Markdown Document" > $NAME.md
-vim $NAME.md
-
+echo "# Title" > $NAME.md
+nvim $NAME.md
 
 exit 0
 fi
+
