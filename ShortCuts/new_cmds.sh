eeec09f2d2b7db94f23c9e4996c810170072af7c
Chris <atnl@protonmail.com>
2024-02-27 16:18:07 -0500

    Create new_cmds.sh

---
+++ ShortCuts/new_cmds.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+: <<DOCUMENTATIONXX
+List the statistics of a specified directory in tabular format.
+# -------------------------------------------------------------
+# This script will print new aliases and commands to the screen
+# I wrote this script because I make so many aliases and shortcuts
+# that I don't always remeber them all
+# Edit this script according to whatever aliases and shortcut commands you create
+# All text is printed in red as dictaed by: "\e[1;31m   \e[0m"
+# You can use that ^^^ as a template for printing text in red,
+#  Just place the text to print between these two "\e[1;31m   \e[0m"
+#  must be used with printf, Not echo
+#  example:
+#  printf "\e[1;31m text to print to the screen  \e[0m" 
+#  headers : List of HTTP request headers.
+#  xfh : List of X-Forwarded for headers.
+#  stealcookies : XSS exploit code to steal cookies.
+#  space : Double spaces lines of a text file.
+#  remap : Remaps caps lock to the escape key.
+#  gprac : cd's to a practice dir for go lang.
+#  showgoimports : prints package main & imports for a .go file.
+#  clte : Content-Length Transfer-Encoding Req Smuggling template.
+#  tecl : Transfer-Encoding Content-Length template.
+#  tete : Transfer-Encoding Transfer-Encoding template.
+#  mongodb-compass : Launch MongoDB desktop Client.
+#  webstorm : Opens the WbStorm IDE.
+#  gol : Launches the GoLand IDE
+#  idea : Launches the IntelliJ Python IDE 
+
+DOCUMENTATIONXX
+
+
+echo ""
+printf "\e[1;31m headers \e[0m"; echo ": List of HTTP request headers."
+
+printf "\e[1;31m xfh \e[0m"; echo  ": List of X-Forwarded for headers."
+
+printf "\e[1;31m stealcookies \e[0m"; echo ": XSS exploit code to steal cookies."
+
+printf "\e[1;31m space \e[0m"; echo ": Double spaces lines of a text file."
+
+printf "\e[1;31m remap \e[0m"; echo ": Remaps caps lock to the escape key."
+
+printf "\e[1;31m gprac \e[0m"; echo  ": cd's to a practice dir for go lang."
+
+printf "\e[1;31m showgoimports \e[0m"; echo  ": prints package main & imports for a .go file."
+
+printf "\e[1;31m clte \e[0m"; echo  ": Content-Length Transfer-Encoding Req Smuggling template."
+
+printf "\e[1;31m tecl \e[0m"; echo  ": Transfer-Encoding Content-Length template."
+
+printf "\e[1;31m tete \e[0m"; echo  ": Transfer-Encoding Transfer-Encoding template."
+
+printf "\e[1;31m mongodb-compass \e[0m"; echo ": Launch MongoDB desktop Client."
+
+printf "\e[1;31m webstorm \e[0m"; echo ": Opens the WbStorm IDE."
+
+printf "\e[1;31m gol \e[0m"; echo  ": Launches the GoLand IDE"
+
+printf "\e[1;31m idea \e[0m"; echo  ": Launches the IntelliJ Python IDE  "
+
+:<<DOCUMENTATIONXX
+# Templates for additional entries
+#printf "\e[1;31m <new cmd here> \e[0m"; echo  ": <what new cmd does> "
+#printf "\e[1;31m <new cmd here> \e[0m"; echo  ": <what new cmd does> "
+#printf "\e[1;31m <new cmd here> \e[0m"; echo  ": <what new cmd does> "
+#printf "\e[1;31m <new cmd here> \e[0m"; echo  ": <what new cmd does> "
+#printf "\e[1;31m <new cmd here> \e[0m"; echo  ": <what new cmd does> "
+
+# This script produces the following outoput:
+# Of course you edit this according to your needs.
+# I add a new entry almost every time I create a new alias, bacuse I've made so many at this point it's hard keeping up with them all
+ 
+ # headers : List of HTTP request headers.
+ # xfh : List of X-Forwarded for headers.
+ # stealcookies : XSS exploit code to steal cookies.
+ # space : Double spaces lines of a text file.
+ # remap : Remaps caps lock to the escape key.
+ # gprac : cd's to a practice dir for go lang.
+ # showgoimports : prints package main & imports for a .go file.
+ # clte : Content-Length Transfer-Encoding Req Smuggling template.
+ # tecl : Transfer-Encoding Content-Length template.
+ # tete : Transfer-Encoding Transfer-Encoding template.
+ # mongodb-compass : Launch MongoDB desktop Client.
+ # webstorm : Opens the WbStorm IDE.
+ # gol : Launches the GoLand IDE
+ # idea : Launches the IntelliJ Python IDE
+
+DOCUMENTATIONXX
+
+echo ""
+
+echo " A copy of this is kept as a dot file in the home directory."
+
+echo""
