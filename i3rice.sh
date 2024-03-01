1d97285a8411dd4a1e0a2ddae891df2f5f06be71
Chris <atnl@protonmail.com>
2024-03-01 17:20:48 -0500

    Delete i3rice.sh

--- i3rice.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get install lxappearance arc-theme moka-icon-theme compton neofetch nitrogen rofi
-
-cd ~ && git clone https://github.com/audrummer15/dotfiles.git
-./dotfiles/install.sh
