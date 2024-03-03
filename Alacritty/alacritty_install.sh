44738c345c80b9d9ad3d8211d7c0aaa97d1b8512
Chris <atnl@protonmail.com>
2024-03-03 02:40:39 -0500

    Update alacritty_install.sh

--- Alacritty/alacritty_install.sh
+++ Alacritty/alacritty_install.sh
@@ -1,22 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Alacritty install script
 # Created By: @LinuxUser255 (Chris)
 # License: GNU GPLv3
 
 # Install shell script for the Alacritty Terminal emulator on Debian & Debian-based distros
-# the goal is to have a one & done easy install for the end user
 #
 # To-Do
 #----------------------------------------------------------------------------------------
-# make sure to include an alacritty.toml config and make the .config/alacritty dir for it.
-# that should be the last step in the Prebuild process
-# integrate the source $HOME/.cargo/env in the rust compiler install function.
-# call all functions in a main function
-# replace all echos with printf in bold red: printf "\e[1;31m Messg here \e[0m\n"
-# Make obvious what part of the script is executing and when
-# printf "\e[1;31m Messg here \e[0m\n"
-# Another dependency to install: scdoc
+# The only two parts not working:
+# man page and autoinstall of alacritty.toml in the ~/.config/alacritty directory
 #---------------------------------------------------------------------------------------
 
 # This is a 2 part install Process:
@@ -42,33 +35,36 @@
 
 #-------------Part 1: Pre-build-------------------------------------------------------#
 
-#
 # First check for sudo privileges, and if so, then proceede
-check_and_install_packages() {
-    # Check if the user has sudo privileges
-    if ! sudo -n true > /dev/null 2>&1; then
+is_sudo() {
+    if [ ${UID} -ne 0 ]; then
+        # ! sudo -n true > /dev/null 2>&1; then
         printf "\e[1;31m Sudo privileges required. \e[0m\n"
         exit 1
     fi
+}
 
-    echo "Conducting full system update and upgrade"
-    sudo apt update && sudo apt upgrade;
+# Install dependencies
+check_and_install_packages() {
 
-    # List of packages to install
-    packages=(
-        curl
-        git
-        cmake
-        scdoc
-        pkg-config
-        libfreetype6-dev
-        libfontconfig1-dev
-        libxcb-xfixes0-dev
-        libxkbcommon-dev
-        python3
-)
- # Check if packages are already installed
-    printf "\e[1;31m Checking dependences \e[0m\n"
+   #dependencies to check for
+   packages=(
+       curl
+       git
+       cmake
+       scdoc
+       neofetch
+       ripgrep
+       pkg-config
+       libfreetype6-dev
+       libfontconfig1-dev
+       libxcb-xfixes0-dev
+       libxkbcommon-dev
+       python3
+   )
+
+    # Check if packages are already installed
+    printf "\e[1;31m Checking dependencies \e[0m\n"
     missing_packages=()
     for package in "${packages[@]}"; do
         if ! dpkg -l | grep -q "^ii\s*$package\s"; then
@@ -82,7 +78,9 @@ check_and_install_packages() {
         printf "\e[1;31m Installing missing packages... \e[0m\n"
         sudo apt update
         sudo apt install -y "${missing_packages[@]}"
-        echo "Installation complete."
+        sudo apt upgrade
+        #echo "Installation complete."
+        printf "\e[1;31m Installation complete. \e[0m\n"
     fi
 }
 
@@ -112,33 +110,15 @@ install_alacritty() {
 
     # Build Alacritty
     cargo build --release
+
+   # configure your current shell
+   source "$HOME/.cargo/env"
+
+   # copy the alacritty binary to path
+   sudo cp -r ~/target/release/alacritty /usr/local/bin
+
+   printf "\e[1;31mAlacritty binary is installed\e[0m\n"
 }
-
-# Function to download alacritty.toml file and move it to ~/.config/alacritty
-download_and_move_alacritty_config() {
-    printf "\e[1;31mDownloading alacritty.toml file...\e[0m\n"
-
-    # Download alacritty.toml file using curl
-    # My custom config
-    #curl -LO https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/Alacritty/alacritty.toml
-
-    # a generic config
-    curl -LO https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/Alacritty/configs/alacritty.toml
-
-    printf "\e[1;31mCreating ~/.config/alacritty directory if it doesn't exist...\e[0m\n"
-
-    # Create the ~/.config/alacritty directory if it doesn't exist
-    mkdir -p ~/.config/alacritty
-
-    printf "\e[1;31mMoving the downloaded alacritty.toml file to ~/.config/alacritty...\e[0m\n"
-
-    # Move the downloaded alacritty.toml file to ~/.config/alacritty
-    mv alacritty.toml ~/.config/alacritty/
-
-    printf "\e[1;31mAlacritty configuration file downloaded and moved successfully.\e[0m\n"
-}
-
-#-------------Part 2: Post-build-------------------------------------------------------#
 
 # Verify and install terminfo for Alacritty
 verify_and_install_terminfo() {
@@ -152,59 +132,80 @@ verify_and_install_terminfo() {
         # Install terminfo globally
         sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
         printf "\e[1;31mTerminfo installed successfully.\e[0m\n"
+
     fi
 }
 
+# Function to download alacritty.toml file and move it to ~/.config/alacritty
+download_and_move_alacritty_config() {
+    printf "\e[1;31mDownloading alacritty.toml file...\e[0m\n"
+
+    # this part needs work, ther's only a default .yml being installed in /etc/xdg/alacritty
+    # Download alacritty.toml file using curl
+    # My custom config
+    #curl -LO https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/Alacritty/alacritty.toml
+    printf "\e[1;31mCreating ~/.config/alacritty directory if it doesn't exist...\e[0m\n"
+
+    #mkdir  ~/.config/alacritty
+    mkdir  "${XDG_CONFIG_HOME:-$HOME/.config/alacritty}"    # Move the downloaded alacritty.toml file to ~/.config/alacritty
+
+    printf "\e[1;31mRetrieving the .toml file from Github alacritty.toml fil\e[0m\n"
+    curl -LO https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/Alacritty/configs/alacritty.toml
+
+    mv alacritty.toml -t ~/.config/alacritty/
+
+    printf "\e[1;31mAlacritty configuration file downloaded and moved successfully.\e[0m\n"
+}
+
+#-------------Part 2: Post-build-------------------------------------------------------#
 
 # Create a Desktop entry for Alacritty
 create_desktop_entry() {
-    echo "Installing Alacritty..."
+    echo "Creating desktop entry..."
 
-    # Change directory to the Alacritty directory
-    cd ~/alacritty || { echo "Error: Alacritty directory not found."; exit 1; }
+    sudo cp target/release/alacritty /usr/local/bin
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
 
-    # Copy Alacritty binary to /usr/local/bin
-    sudo cp target/release/alacritty /usr/local/bin || { echo "Error: Failed to copy Alacritty binary."; exit 1; }
-
-    # Copy Alacritty logo to /usr/share/pixmaps/
-    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg || { echo "Error: Failed to copy Alacritty logo."; exit 1; }
-
-    # Install Alacritty Desktop entry
-    sudo desktop-file-install extra/linux/Alacritty.desktop || { echo "Error: Failed to install Alacritty Desktop entry."; exit 1; }
-
-    # Update desktop database
-    sudo update-desktop-database || { echo "Error: Failed to update desktop database."; exit 1; }
-
-    echo "Alacritty installed successfully."
+    echo "Desktop entry installed successfully."
 }
-
 
 # Create the Alacritty manual page
 create_alacritty_manual_page() {
+
+    # This part's not working either
     echo "Creating Alacritty manual page..."
+    printf "\e[1;31m Creating Alacritty manual page.\e[0m\n"
 
     # Change directory to the Alacritty directory
-    cd ~/alacritty || { echo "Error: Alacritty directory not found."; exit 1; }
+    #cd ~/alacritty || { echo "Error: Alacritty directory not found."; exit 1; }
 
     # Create alacritty manual page directory if it doesn't exist
-    sudo mkdir -p /usr/local/share/man/man1 || { echo "Error: Failed to create manual page directory."; exit 1; }
+    sudo mkdir -p /usr/local/share/man/man1 #|| { echo "Error: Failed to create manual page directory."; exit 1; }
 
     # Create and install alacritty manual pages
-    sudo sh -c "gzip -c extra/alacritty.man | tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null" || { echo "Error: Failed to create alacritty manual page."; exit 1; }
-    sudo sh -c "gzip -c extra/alacritty-msg.man | tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null" || { echo "Error: Failed to create alacritty-msg manual page."; exit 1; }
+    sudo sh -c "gzip -c extra/alacritty.man | tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null" #|| { echo "Error: Failed to create alacritty manual page."; exit 1; }
+    sudo sh -c "gzip -c extra/alacritty-msg.man | tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null" #|| { echo "Error: Failed to create alacritty-msg manual page."; exit 1; }
+
+   # NOT working -> gzip: extra/alacritty.man: No such file or directory
 
     echo "Alacritty manual pages created and installed successfully."
 }
-
 
 # Implement automatic completions for Alacritty's flags and arguments
 implement_completions() {
     printf "\e[1;31m Implementing auto completions for Alacritty's flags and args \e[0m\n"
 
     # Check which shell is in use: Zsh, Bash, Fish
-    if [ -n "$ZSH_VERSION" ]; then
-        # If Zsh, install completions for zsh
+    current_shell="$(neofetch | rg -i "zsh" | awk '{print $2}')"
+    if [[ "$current_shell" = z* ]];
+    then
         printf "\e[1;31m Detected Zsh shell. \e[0m\n"
+
+    #if [ -n "$ZSH_VERSION" ]; then
+    # If Zsh, install completions for zsh
+    printf "\e[1;31m Detected Zsh shell. \e[0m\n"
 
         # Create directory for Zsh completions if not already present
         mkdir -p ${ZDOTDIR:-~}/.zsh_functions
@@ -213,14 +214,16 @@ implement_completions() {
         # Copy completion file to Zsh directory
         cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
 
-    elif [ -n "$BASH_VERSION" ]; then
+    elif current_shell="$(neofetch | rg -i "bash" | awk '{print $2}')"
+    then
         # If Bash, install completions for bash
         printf "\e[1;31m Detected Bash \e[0m\n"
 
         # Source the completion file in ~/.bashrc
         echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
 
-    elif [ -n "$fish" ]; then
+    elif current_shell="$(neofetch | rg -i "fish" | awk '{print $2}')"
+    then
         # If Fish, install completions for fish
         printf "\e[1;31m Detected the Fish shell. \e[0m\n"
 
@@ -245,7 +248,7 @@ implement_completions() {
 
 # Execute script with the main function
 main() {
-    printf "\e[1;31m Starting the install script. \e[0m\n"
+    is_sudo
 
     check_and_install_packages
 
@@ -262,8 +265,6 @@ main() {
     create_alacritty_manual_page
 
     implement_completions
-
-    printf "\e[1;31m Installation complete. \e[0m\n"
 }
 
 main
