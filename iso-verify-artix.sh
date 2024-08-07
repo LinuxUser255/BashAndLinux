1de2be832171d72a088641cbfea61c5059280cf1
Chris <atnl@protonmail.com>
2024-08-07 17:08:23 -0400

    Update iso-verify-artix.sh

--- iso-verify-artix.sh
+++ iso-verify-artix.sh
@@ -9,6 +9,28 @@
 # Artix home page: https://artixlinux.org/
 # Download: https://artixlinux.org/download.php
 
+# -- Also this --- #
+# Artix gpg keyring server
+# https://pgp.circl.lu/pks/lookup?search=0xB886B428&fingerprint=on&op=index
+# You will need to import the key:
+gpg --keyserver  hkps://pgp.circl.lu  --recv 0xB886B428
+
+# response should say:
+gpg: key 606520ACB886B428: public key "Christos Nouskas <nous@artixlinux.org>" imported
+gpg: Total number processed: 1
+gpg:               imported: 1
+
+# Then do:
+gpg --auto-key-retrieve --verify artix-lxde-runit-20230814-x86_64.iso.sig  artix-lxde-runit-20230814-x86_64.iso
+gpg: Signature made Thu 17 Aug 2023 04:23:11 PM EDT
+gpg:                using RSA key A574A1915CEDE31A3BFF5A68606520ACB886B428
+gpg: Good signature from "Christos Nouskas <nous@artixlinux.org>" [unknown]
+gpg: WARNING: This key is not certified with a trusted signature!
+gpg:          There is no indication that the signature belongs to the owner.
+Primary key fingerprint: A574 A191 5CED E31A 3BFF  5A68 6065 20AC B886 B428
+
+# That should be the response you get ^^^^^^^^^^^
+
 # 1. Retrieve All Artix devs gpg keys fom github
 wget https://raw.githubusercontent.com/artix-linux/artix-keyring/master/artix.gpg
 
