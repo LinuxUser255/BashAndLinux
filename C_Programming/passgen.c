bb84540a6663f3d15b2d2b987d7c3af47494fccd
Chris <atnl@protonmail.com>
2024-08-21 23:00:59 -0400

    Create passgen.c

---
+++ C_Programming/passgen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Random password generator
+#define PASSWORD_LENGTH 20  // Define the length of the password
+#define NUM_CHARACTERS (sizeof(characters) - 1)  // Number of available characters
+
+int main(void) {
+    // Available characters for the password
+    const char characters[] = "abcdefghijklmnopqrstuvwxyz"
+                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                              "0123456789"
+                              "!@#$%^&*()_+[]{}|;:,.<>?";
+                              
+    char password[PASSWORD_LENGTH + 1];  // Array to hold the generated password
+    srand((unsigned int)time(NULL));  // Seed the random number generator
+
+    // Generate the random password
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = characters[rand() % NUM_CHARACTERS];
+    }
+    password[PASSWORD_LENGTH] = '\0';  // Null-terminate the password string
+
+    // Print the generated password
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
