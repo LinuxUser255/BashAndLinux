bb0f3eafa7c6c2ef9faeff2a1f4d79d68b98fd2c
Chris <atnl@protonmail.com>
2024-08-29 15:15:54 -0400

    Create cloner.py

---
+++ EasyCloner/cloner.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+
+# python script that iterates over a list of git repositories, in repos.txt, and git clone every one
+
+import os
+
+# Read repositories from repos.txt
+with open('repos.txt', 'r') as f:
+    repos = f.read().splitlines()
+
+    # Iterate over each repository and clone it to the PWD
+    for repo in repos:
+        print(f'Cloning {repo}...')
+        os.system(f'git clone {repo}')
+        print('Done.')
